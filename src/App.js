3bb67bf9c312131877a52ac280eeae577693ae6a
Your Name <etui@example.com>
2021-04-09 18:18:36 +0300

    Fcomponent

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Fcomponent from './Fcomponent'
 
-function App() {
-    return ( <
-        div className = "App" >
-        <
-        header className = "App-header" >
-        <
-        img src = { logo }
-        className = "App-logo"
-        alt = "logo" / >
-        <
-        p > Edit < code > src / App.js < /code> and save to reload. < /p >
-        <
-        div > Hello!My name is Anastasia.This is my first application!!! < /div>  <
-        a className = "App-link"
-        href = "https://reactjs.org"
-        target = "_blank"
-        rel = "noopener noreferrer" >
-        Learn React <
-        /a> < /
-        header > <
-        /div>
+function App(props) {
+    const subject="React";
+    console.log(props);
+    return ( <div className="App" >
+        <header className="App-header">
+        <Fcomponent /> 
+        <img src={ logo }
+        className="App-logo"
+        alt="logo"/>
+        <p> Hello, { subject }! </p>  
+        <div > Hello!My name is Anastasia.This is my first application!!! </div>   
+        <a className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer">
+        Learn React </a> </header> </div>
     );
 }
+
 
 export default App;
